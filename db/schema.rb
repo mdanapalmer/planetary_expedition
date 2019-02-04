5257cb7934f2b4c8a2cd1782ec169d9847cc95f6
github username <github email address>
2019-02-03 22:27:12 -0700

    added password column

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_04_052203) do
+ActiveRecord::Schema.define(version: 2019_02_04_052344) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
+    t.string "password_digest"
   end
 
 end
