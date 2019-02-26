b5a6272f210c9c0994b86c71efbf65daa07e840b
github username <github email address>
2019-02-26 00:37:19 -0700

    working on associations

--- app/controller/astronauts_controller.rb
+++ app/controller/astronauts_controller.rb
@@ -1,67 +1,66 @@
 class AstronautsController < ApplicationController
 
-	get '/astronauts/new' do
-		if logged_in?
-			@astronaut = Astronaut.new
-			erb :'astronauts/new.html'
-		else
-			erb :'error'
-		end
-	end
+  get '/astronauts/new' do
+    if logged_in?
+      @astronaut = Astronaut.new
+      erb :'astronauts/new.html'
+    else
+      erb :'error'
+    end
+  end
 
-	get '/astronauts/:id/edit' do
-		if  logged_in?
-			@astronaut = Astronaut.find(params[:id])
-			erb :'astronauts/edit.html'
-		else
-			erb :'error'
-		end
-	end
+  get '/astronauts/:id/edit' do
+    if logged_in? &&
+      @astronaut = Astronaut.find(params[:id])
+      erb :'astronauts/edit.html'
+    else
+      erb :'error'
+    end
+  end
 
-	get '/astronauts/:id' do
-		if  logged_in?
-			@astronaut = Astronaut.find(params[:id])
-			erb :'astronauts/show.html'
-		else
-			erb :'error'
-		end
-	end
+  get '/astronauts/:id' do
+    if logged_in?
+      @astronaut = Astronaut.find(params[:id])
+      erb :'astronauts/show.html'
+    else
+      erb :'error'
+    end
+  end
 
-	patch '/astronauts/:id' do 
-		if logged_in?
-			@astronaut = Astronaut.find(params[:id])
-			@astronaut.update(params[:astronaut])    
-			redirect to "/astronauts/#{@astronaut.id}"
-		else
-			erb :'error'
-		end
-	end
+  patch '/astronauts/:id' do 
+    if logged_in?
+      @astronaut = Astronaut.find(params[:id])
+      @astronaut.update(params[:astronaut])    
+      redirect to "/astronauts/#{@astronaut.id}"
+    else
+      erb :'error'
+    end
+  end
 
-	delete "/astronauts/:id" do
-		if logged_in?
-			Astronaut.destroy(params[:id])
-			redirect to "/astronauts"
-		else
-			erb :'error'
-		end
-	end
+  delete "/astronauts/:id" do
+    if logged_in?
+      Astronaut.destroy(params[:id])
+      redirect to "/astronauts"
+    else
+      erb :'error'
+    end
+  end
 
-	get "/astronauts" do
-		if logged_in?
-			@astronauts = current_user.astronauts  
-			erb :'astronauts/index.html'
-		else
-			erb :'error'
-		end
-	end 
+  get "/astronauts" do
+    if logged_in?
+      @astronauts = current_user.astronauts  
+      erb :'astronauts/index.html'
+    else
+      erb :'error'
+    end
+  end 
 
-	post "/astronauts" do
-		@user = current_user
-		if logged_in?
-			@astronaut = Astronaut.create(user_id: current_user.id, name: params[:name])#, weight, height, bio, nationality, specialty )
-			erb :"/astronauts/show.html"
-		else
-			erb :'error'
-		end
-	end  
+  post "/astronauts" do
+      if logged_in?
+        @astronaut = Astronaut.create(user_id: current_user.id, name: params[:name], weight: params[:weight], height: params[:height], bio: params[:bio], nationality: params[:nationality], specialty: params[:specialty], ship: params[:ship])
+        erb :'astronauts/show.html'
+      else
+        erb :'error'
+      end
+  end  
 end
--- app/controller/spaceships_controller.rb
+++ app/controller/spaceships_controller.rb
@@ -1,74 +1,72 @@
 class SpaceshipsController < ApplicationController
 
 # new
-	get '/spaceships/new' do
-		if logged_in?
-		erb :'spaceships/new.html'
-		else
-		erb :'error'
-	end
-end
+  get '/spaceships/new' do
+    if logged_in?
+      erb :'spaceships/new.html'
+    else
+      erb :'error'
+    end
+  end
 
-	# show
-	get "/spaceships/:id" do  
-		if logged_in?
-			@spaceship = Spaceship.find(params[:id])
-		erb :'spaceships/show.html'
-		else
-		erb :'error'
-	end
-end
+  # show
+  get "/spaceships/:id" do  
+    if logged_in?
+      @spaceship = Spaceship.find(params[:id])
+      erb :'spaceships/show.html'
+    else
+      erb :'error'
+    end
+  end
 
-		# edit
-	get '/spaceships/:id/edit' do
-		if logged_in?
-			@spaceship = Spaceship.find(params[:id])
-		erb :'spaceships/edit.html'
-		else
-		erb :'error'
-	end
-end
+  # edit
+  get '/spaceships/:id/edit' do
+    if logged_in?
+      @spaceship = Spaceship.find(params[:id])
+      erb :'spaceships/edit.html'
+    else
+      erb :'error'
+    end
+  end
 
-	# index
-	get '/spaceships' do
-		if logged_in?
-			@spaceships = current_user.spaceships     
-		erb :'spaceships/index.html'
-		else
-		erb :'error'
-	end
-end
+  # index
+  get '/spaceships' do
+    if logged_in?
+      @spaceships = current_user.spaceships     
+      erb :'spaceships/index.html'
+    else
+      erb :'error'
+    end
+  end
 
-	# create
-	post '/spaceships' do
-		if logged_in?
-			@spaceship = Spaceship.create(params[:spaceship])
-			@spaceship.user_id = current_user.id
-		erb :'spaceships/show.html'
-		else
-		erb :'error'
-	end  
-end
+  # create
+  post '/spaceships' do
+    if logged_in?
+      @spaceship = Spaceship.create(user_id: current_user.id, name: params[:name], ship_type: params[:ship_type], speed: params[:speed], cargo: params[:cargo], crew_member: params[:crew_member])
+      erb :'spaceships/show.html'
+    else
+      erb :'error'
+    end  
+  end
 
-	# update
-	patch '/spaceships/:id' do
-		if logged_in?
-			@spaceship = Spaceship.find(params[:id])
-			@spaceship.update!(params[:spaceship])
-		redirect to "/spaceships/#{@spaceship.id}"
-		else
-		erb :'error'
-	end
-end
+  # update
+  patch '/spaceships/:id' do
+    if logged_in?
+      @spaceship = Spaceship.find(params[:id])
+      @spaceship.update!(params[:spaceship])
+      redirect to "/spaceships/#{@spaceship.id}"
+    else
+      erb :'error'
+    end
+  end
 
-	#destroy
-	delete "/spaceships/:id" do
-		if logged_in?
-		Spaceship.destroy(params[:id])
-		redirect to "/spaceships"
-	else
-		erb :'error'
-	end
-end
-
+  #destroy
+  delete "/spaceships/:id" do
+    if logged_in?
+      Spaceship.destroy(params[:id])
+      redirect to "/spaceships"
+    else
+      erb :'error'
+    end
+  end
 end
